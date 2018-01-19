6ca6bd1f23f966037e09d987e95920dc8f56dc12
Tyler Wickline <twick00@gmail.com>
2018-01-19 13:19:26 -0800

    Finish back-end logic.

--- js/scripts.js
+++ js/scripts.js
@@ -1,50 +1,81 @@
 $(document).ready(function () {
     $("#beep-boop-form").submit(function (event) {
-        event.preventDefault();
-        var userInput;
-        if (parseInt($("#beep-boop-button").val()) < 0) {
-            console.log("Please enter a positive number");
-        } else if (parseInt($("#beep-boop-button").val()) > 0){
-            main(parseInt($("#beep-boop-button").val()));
-            console.log(userInput);
-        } else {
-            //Output: error, not a number
-            console.log("Not a number");
+        var userName;
+        if ($("#input-name").val() != "") {
+            userName = $("#input-name").val();
         }
+        event.preventDefault();
+        $("#output").text("");
+        var userInput;
+        var userOutput;
+        if (parseInt($("#beep-boop-button").val()) < 0) {
+            $("#output").append("Please enter a positive number");
+        } else if (parseInt($("#beep-boop-button").val()) > 0) {
+            userOutput = main(parseInt($("#beep-boop-button").val()), userName);
+            
+        } else {
+            $("#output").append("Please enter a number");
+        }
+        if (userOutput != undefined) {
+            var textArray = [];
+            userOutput.forEach(function (number, index, numberArray) {
+                    $("#output").append(appendList(number));
+
+            });
+        }
+        $(".results").show();
     });
 });
 
-function main(userInput) {
+function appendList(numbers) {
+    return '<li>' + numbers + '</li>';
+}   
+
+function main(userInput, userName) {
     var numberArray = [];
     for (var i = 1; i <= userInput; i++) {
         numberArray.push(i);
     }
-    userName = "Dave";
-    numberArray.forEach(function(number) {
+    if (userName == undefined) {
+        userName = "Dave";
+    }
+    numberArray.forEach(function (number, index, numberArray) {
+        var tempString;
         for (var i = 0; i < number.toString().length; i++) {
-            var tempString;
-            if (number[i] == "0") {
-                console.log(number[i]);
-                tempString += "Beep";
-            }
-            if (number[i] == "1") {
-                if (tempString == undefined) {
-                    tempString += "Boop!";
-                }
-                else {
-                    tempString += " Boop!"; //Adds space for good grammar.
-                }
-            }
-            if (number[i] % 3) {
-                if (tempString == undefined) {
-                    tempString += ("I'm sorry, " + userName + ". I'm afraid I can't do that.");
-                }
-                else {
-                    tempString += " I'm sorry, " + userName + ". I'm afraid I can't do that."
-                    //Adds space for good grammar.
-                }
-            }
-            console.log(tempString);
+            tempString = checkBeep(number, i, tempString);
+            tempString = checkBoop(number, i, tempString);
+            tempString = checkSorry(number, tempString, userName);
+        }
+        if (tempString != undefined) {
+            numberArray[index] = tempString;
         }
     });
+    return numberArray;
+}
+
+function checkBeep(number, i, tempString) {
+    if (number.toString()[i] == 0) {
+        if (tempString == undefined) {
+            tempString = "Beep!";
+        }
+    }
+    return tempString;
+}
+
+function checkBoop(number, i, tempString) {
+    if (number.toString()[i] == 1) {
+        if (tempString == undefined) {
+            tempString = "Boop!";
+        }
+    }
+    return tempString;
+}
+
+function checkSorry(number, tempString, userName) {
+    if (number.toString() % 3 == 0) {
+        if (tempString == undefined) {
+            tempString = (" I'm sorry, " + userName + ". I'm afraid I can't do that. ");
+        }
+    }
+    return tempString;
 }
